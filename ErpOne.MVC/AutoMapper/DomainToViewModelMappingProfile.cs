c648f3b6655e172034c30a4e9a36eb219fe58c74
Suykoden <damasio321@hotmail.com>
2016-07-19 13:09:20 -0300

    AutoMapper

---
+++ ErpOne.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ErpOne.Domain.Entities;
+using ErpOne.MVC.ViewModels;
+
+namespace ErpOne.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Cliente, ClienteViewModel>();
+        }
+    }
+}
---
+++ ErpOne.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ErpOne.Domain.Entities;
+using ErpOne.MVC.ViewModels;
+
+namespace ErpOne.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile: Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<ClienteViewModel, Cliente>();
+
+        }
+    } 
+   
+}
