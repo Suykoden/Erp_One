10709f17dc3d57367a0c7e60f78d1b5813fb3946
Suykoden <damasio321@hotmail.com>
2016-09-06 16:23:40 -0300

    API

--- ErpOne.MVC/Controllers/AnunciantesController.cs
+++ ErpOne.MVC/Controllers/AnunciantesController.cs
@@ -7,31 +7,31 @@ using System.Web.Http;
 
 namespace ErpOne.MVC.Controllers
 {
-    public class APIController : ApiController
+    public class AnunciantesController : ApiController
     {
-        // GET: api/API
+        // GET: api/Anunciantes
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/API/5
+        // GET: api/Anunciantes/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/API
+        // POST: api/Anunciantes
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT: api/API/5
+        // PUT: api/Anunciantes/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/API/5
+        // DELETE: api/Anunciantes/5
         public void Delete(int id)
         {
         }
